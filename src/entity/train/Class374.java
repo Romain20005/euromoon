0cb0885bcf0c6ba1b477b3fd0f440081760d6fe7
Romain20005 <romain.musungayi@student.ehb.be>
2026-01-13 22:22:21 +0100

    Klasse locomotief 374(14 wagons)

---
+++ src/entity/train/Class374.java
@@ -0,0 +1,10 @@
+package entity.train;
+
+/**
+ * Class 374 locomotief die maximum 14 wagons heeft
+ */
+public class Class374 extends Locomotief {
+    public Class374(){
+        super(12);
+    }
+}
