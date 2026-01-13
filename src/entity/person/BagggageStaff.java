9ba9a6d2880be6d6837efb05487cced2df76cb95
Romain20005 <romain.musungayi@student.ehb.be>
2026-01-13 21:58:00 +0100

    Class bagage

---
+++ src/entity/person/BagggageStaff.java
@@ -0,0 +1,10 @@
+package entity.person;
+
+/**
+ * Personeel van het bagage dienst
+ */
+public class BagggageStaff extends Staff {
+    public BagggageStaff(String firstName, String lastName, String nationalRegisterNumber, String birthDate){
+        super(firstName, lastName, nationalRegisterNumber, birthDate);
+    }
+}
