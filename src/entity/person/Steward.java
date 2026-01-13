3da2204bf7073d210c8f4ee058d77655fa249b4f
Romain20005 <romain.musungayi@student.ehb.be>
2026-01-13 21:51:33 +0100

    Class steward

---
+++ src/entity/person/Steward.java
@@ -0,0 +1,10 @@
+package entity.person;
+
+/**
+ * dDit representeert een steward.
+ */
+public class Steward extends Staff {
+    public Steward(String firstName, String lastName, String nationalRegisterNumber, String birthDate){
+        super(firstName, lastName, nationalRegisterNumber, birthDate);
+    }
+}
