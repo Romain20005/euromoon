c1917356a7421fc390f07c091eec2aee69263478
Romain20005 <romain.musungayi@student.ehb.be>
2026-01-14 00:22:12 +0100

    Ticket klasse

---
+++ src/entity/tickect/Ticket.java
@@ -0,0 +1,23 @@
+package entity.tickect;
+import entity.person.Passanger;
+import entity.trip.Trip;
+public class Ticket {
+    private Passanger passanger;
+    private Trip trip;
+    private boolean firstClass;
+
+    public Ticket(Passanger passanger, Trip trip, boolean firstClass) {
+        this.passanger = passanger;
+        this.trip = trip;
+        this.firstClass = firstClass;
+    }
+    public Passanger getPassanger() {
+        return passanger;
+    }
+    public Trip getTrip() {
+        return trip;
+    }
+    public boolean isFirstClass() {
+        return firstClass;
+    }
+}
